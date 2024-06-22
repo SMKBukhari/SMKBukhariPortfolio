908d8ad3658f7651fb4171ca1e858b839ebe8512
Syed Muhammad Kaif Bukhari <bc240200869smk@vu.edu.pk>
2024-06-22 05:50:16 +0500

    Some Changes
    PersonalData, Skills, Experiences Added

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
   {
     id: 1,
+    title: "Frontend Developer - (Internship)",
+    company: "ValleyWays Tech, Rawalpindi, Pakistan",
+    duration: "(May 2024 - Present)"
+  },
+  {
+    id: 2,
     title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "(Jan 2018 - Present)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Video Editor",
     company: "The Truth International, Islamabad, Pakistan",
     duration: "(Jun 2023 - Jan 2024)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Computer Instructor - Advance IT Course",
     company: "Students Welfare Society, Rawalpindi, Pakistan",
     duration: "(Jan 2020 - Aug 2022)"
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,20 @@
 export const personalData = {
   name: "Syed Muhammad Kaif Bukhari",
-  profile: '/profile5.jpg',
-  designation: "Website Developer",
-  description: "My name is Syed Muhammad Kaif Bukhari. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 's.mkb2524@outlook.com',
-  phone: '+923259090028',
-  address: 'Rawalpindi, Pakistan',
-  github: 'https://github.com/SMKBukhari',
-  facebook: '',
-  linkedIn: 'https://www.linkedin.com/in/smkbukhari/',
-  twitter: '',
-  stackOverflow: 'https://stackoverflow.com/users/24885198/syed-muhammad-kaif-bukhari',
+  profile: "/profile5.jpg",
+  designation: "MERN Stack Developer",
+  description:
+    "My name is Syed Muhammad Kaif Bukhari. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "s.mkb2524@outlook.com",
+  phone: "+923259090028",
+  address: "Rawalpindi, Pakistan",
+  github: "https://github.com/SMKBukhari",
+  facebook: "",
+  linkedIn: "https://www.linkedin.com/in/smkbukhari/",
+  twitter: "",
+  stackOverflow:
+    "https://stackoverflow.com/users/24885198/syed-muhammad-kaif-bukhari",
   leetcode: "https://leetcode.com/u/MuhammadKaif5515/",
   devUsername: "smkbukhari",
-  resume: "https://drive.google.com/file/d/1td-51xSyLkINLkzY2EF_l34sG5xacXzp/view?usp=sharing"
-}
+  resume:
+    "https://drive.google.com/file/d/1td-51xSyLkINLkzY2EF_l34sG5xacXzp/view?usp=sharing",
+};
