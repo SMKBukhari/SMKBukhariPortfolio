2dbaf45e6f12f6f720c730643765a1bbbd51d980
Syed Muhammad Kaif Bukhari <bc240200869smk@vu.edu.pk>
2024-08-18 12:55:16 +0500

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,10 +1,11 @@
 export const experiences = [
   {
     id: 1,
-    title: "Frontend Developer - (Internship)",
-    company: "ValleyWays Tech, Rawalpindi, Pakistan",
-    duration: "(May 2024 - Present)"
+    title: "Full Stack Developer - (Remote Intern)",
+    company: "Devsinz, Karachi, Pakistan",
+    duration: "(Jun 2024 - Present)"
   },
+  
   {
     id: 2,
     title: "Self Employed",
@@ -15,10 +16,16 @@ export const experiences = [
     id: 3,
     title: "Video Editor",
     company: "The Truth International, Islamabad, Pakistan",
-    duration: "(Jun 2023 - Jan 2024)"
+    duration: "(Jun 2023 - Present)"
   },
   {
     id: 4,
+    title: "Frontend Developer - (Internship)",
+    company: "ValleyWays Tech, Rawalpindi, Pakistan",
+    duration: "(May 2024 - Aug 2024)"
+  },
+  {
+    id: 5,
     title: "Computer Instructor - Advance IT Course",
     company: "Students Welfare Society, Rawalpindi, Pakistan",
     duration: "(Jan 2020 - Aug 2022)"
